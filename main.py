0ca1ba6f64cb524483e008c0cec13e07f8099184
liriumgames <yosefedery@hotmail.es>
2023-06-08 14:18:26 +0300

    df

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def create_guest_same_country_booking(df):
 
 
 def add_features(df):
-    df["date_to_checkin"] = apply_booking_date(df)
+    df["weeks_to_checkin"] = apply_booking_date(df)
     df["duration_trip"] = create_trip_duration(df)
     df["same_country"] = create_guest_same_country_booking(df)
     return df
@@ -36,7 +36,7 @@ def add_features(df):
 
 def create_cancellation_colunmn(df):
     date_cancellation = df["cancellation_datetime"]
-    df["cancellation"] = date_cancellation.isnull().astype(int)
+    return date_cancellation.isnull().astype(int)
 
 
 def decode_policy(code):
@@ -62,7 +62,7 @@ def days_difference(booking_date_str, check_in_date_str):
     check_in_date = datetime.strptime(check_in_date_str, date_format)
 
     difference = check_in_date - booking_date
-    return difference.days
+    return round((difference.days + 1) / 7)
 
 
 def transform_to_binary(df):
@@ -76,7 +76,6 @@ def preprocess_data(df):
     df = drop_useless_columns(df)
     df = add_features(df)
     df = classify_columns(df)
-    df = transform_to_binary(df)
     return df
 
 def classify_columns(df):
@@ -87,3 +86,11 @@ if __name__ == "__main__":
     np.random.seed(0)
     df = pd.read_csv("agoda_cancellation_train.csv")
     df = preprocess_data(df)
+    y = create_cancellation_colunmn(df)
+    X = df.drop(columns=["cancellation_datetime"])
+    a = 1
+    # create_cancellation_colunmn(df)
+    # print(apply_booking_date(df))
+
+
+    # print(df)
