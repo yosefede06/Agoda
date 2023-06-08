2ff88a6e4dab1ead96d5b85cfc2633aab9c438be
KerenSfez2304 <94888143+KerenSfez2304@users.noreply.github.com>
2023-06-08 17:57:06 +0300

    add policy

--- main.py
+++ main.py
@@ -14,42 +14,89 @@ DROP_COLUMNS = [ "hotel_id", "customer_nationality", 'no_of_adults', "no_of_chil
 DUMMIES_COLUMNS = ['hotel_country_code', 'accommadation_type_name',
                    'original_payment_method','original_payment_type', 'charge_option']
 
+
 def drop_useless_columns(df):
+    """
+    Removes irrelevant or non-correlated columns (s.t "hotel_id", "customer_nationality")
+    that were not expected to significantly impact cancellation predictions.
+    :param df: Dataframe, Design matrix of regression problem
+    :return: df
+    """
     df = df.drop(columns=DROP_COLUMNS)
     return df
 
 
 def apply_booking_date(df):
+    """
+    create a new column that represents the number of weeks from the booking date to the checkin date
+    :param df: Dataframe, Design matrix of regression problem
+    :return: df
+    """
     return df.apply(lambda row: days_difference(row['booking_datetime'], row['checkin_date']), axis=1)
 
 def create_for_free_cancelation(df):
+    """
+    create a new column that represents the number of weeks from the booking date until the end of the free cancelation period
+    :param df: Dataframe, Design matrix of regression problem
+    :return: df
+    """
     df = df.drop(df[df["cancellation_policy_code"] == "UNKNOWN"].index)
-    return df.apply(lambda row: days_difference(row['booking_datetime'], row['checkin_date']) -
-                                max(round((max(item['days'] for item in decode_policy(row['cancellation_policy_code'])) + 1)/7), 0), axis=1)
+    return df.apply(lambda row: max(days_difference(row['booking_datetime'], row['checkin_date']) -
+                                round((max(item['days'] for item in decode_policy(row['cancellation_policy_code'])) + 1)/7), 0), axis=1)
 
 
 def create_trip_duration(df):
+    """
+    create a new column that represents the trip duration (in weeks - upper rounded)
+    :param df: Dataframe, Design matrix of regression problem
+    :return: df
+    """
     return df.apply(lambda row: days_difference(row['checkin_date'], row['checkout_date']), axis=1)
 
 
 def create_guest_same_country_booking(df):
+    """
+    create a new column that takes the value True if the guest is in the same country than the hostel, False otherwise.
+    :param df: Dataframe, Design matrix of regression problem
+    :return: df
+    """
     return np.where(df['hotel_country_code'] == df['origin_country_code'], 1, 0)
 
 
 def add_features(df):
+    """
+    add 4 new features:
+    "weeks_to_checkin": Calculated the number of weeks between the booking date and the check-in date, providing insights into the time gap.
+    "duration_trip": Computed the duration of the trip by subtracting the check-in date from the checkout date, capturing the length of the stay.
+    "same_country": True if the guest is from the booking country, False otherwise
+    "time_for_free_cancellation": Determined the remaining time for free cancellation based on the check-in date and hotel's cancellation policy.
+    :param df: Dataframe, Design matrix of regression problem
+    :return: df
+    """
     df["weeks_to_checkin"] = apply_booking_date(df)
     df["duration_trip"] = create_trip_duration(df)
     df["same_country"] = create_guest_same_country_booking(df)
     df["time_for_free_cancellation"] = create_for_free_cancelation(df)
+
     return df
 
 
 def create_cancellation_colunmn(df):
+    """
+    create the cancellation column
+    :param df: Dataframe, Design matrix of regression problem
+    :return: df
+    """
     date_cancellation = df["cancellation_datetime"]
     return date_cancellation.isnull().astype(int)
 
 
 def decode_policy(code):
+    """
+    decode the policy column into list of dictionaries with day, night and price of cancellation
+    :param code: the policy string
+    :return:
+    """
     data = []
     policies = code.split("_")
     for pol in policies:
@@ -66,6 +113,12 @@ def decode_policy(code):
 
 
 def days_difference(booking_date_str, check_in_date_str):
+    """
+    computes the difference between two dates in weeks (upper rounded)
+    :param booking_date_str: the first date
+    :param check_in_date_str: the second date
+    :return: the number of weeks (upper rounded) between the two dates
+    """
     date_format = "%Y-%m-%d %H:%M:%S"
 
     booking_date = datetime.strptime(booking_date_str, date_format)
@@ -76,12 +129,26 @@ def days_difference(booking_date_str, check_in_date_str):
 
 
 def transform_to_binary(df):
+    """
+    transform True/False features to binary values
+    :param df: Dataframe, Design matrix of regression problem
+    :return: df
+    """
     df["is_first_booking"] = df["is_first_booking"].astype(int)
     df["is_user_logged_in"] = df["is_user_logged_in"].astype(int)
     return df
 
 
 def preprocess_data(df):
+    """
+    preprocess the data:
+    - Removes irrelevant or non-correlated columns
+    - Introduces new features
+    - Converts categorical variables into dummy variables
+    - Transforms true/false columns into binary values
+    :param df: Dataframe, Design matrix of regression problem
+    :return: df
+    """
     df = drop_useless_columns(df)
     df = add_features(df)
     df = df.drop(columns=["origin_country_code", "booking_datetime", "checkin_date", "checkout_date", "hotel_live_date", "h_booking_id"])
@@ -91,6 +158,11 @@ def preprocess_data(df):
 
 
 def classify_columns(df):
+    """
+    Converts categorical variables into dummy variables
+    :param df: Dataframe, Design matrix of regression problem
+    :return: df
+    """
     return pd.get_dummies(df, columns=DUMMIES_COLUMNS, dtype=float)
 
 
@@ -119,6 +191,10 @@ def misclassification_error(y_true: np.ndarray, y_pred: np.ndarray, normalize: b
     return false_response
 
 def clean_data():
+    """
+    cleans the data and preprocess it
+    :return:
+    """
     np.random.seed(0)
     df = pd.read_csv("agoda_cancellation_train.csv")
     df = preprocess_data(df)
