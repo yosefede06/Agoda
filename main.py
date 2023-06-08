6b6a1f5faec98a91a126bac4a99db9954cc99a4e
liriumgames <yosefedery@hotmail.es>
2023-06-08 16:30:48 +0300

    df

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 from pathlib import Path
+from sklearn import ensemble
+import matplotlib.pyplot as plt
+
 COLUMNS_POLICY = {"D": "days", "N": "nights", "P": "price"}
 DROP_COLUMNS = [ "hotel_id", "customer_nationality", 'no_of_adults', "no_of_children", "no_of_room",
                'guest_nationality_country_name', 'language','original_payment_currency',
                 'request_nonesmoke','request_latecheckin','request_highfloor','request_largebed',
                 'request_twinbeds','request_airport','request_earlycheckin', "hotel_brand_code", "hotel_chain_code",
-                 'hotel_area_code','hotel_city_code']
+                 'hotel_area_code','hotel_city_code', "h_customer_id", "cancellation_policy_code"]
 DUMMIES_COLUMNS = ['hotel_country_code', 'accommadation_type_name',
                    'original_payment_method','original_payment_type', 'charge_option']
 
@@ -55,7 +58,6 @@ def decode_policy(code):
             if col in pol:
                 temp = pol.split(col)
                 new_data[COLUMNS_POLICY[col]] = int(temp[0])
-                # print("_________", temp[0])
                 pol = "".join(temp[1:])
             else:
                 new_data[COLUMNS_POLICY[col]] = -1
@@ -82,26 +84,54 @@ def transform_to_binary(df):
 def preprocess_data(df):
     df = drop_useless_columns(df)
     df = add_features(df)
+    df = df.drop(columns=["origin_country_code", "booking_datetime", "checkin_date", "checkout_date", "hotel_live_date", "h_booking_id"])
     df = classify_columns(df)
     df = transform_to_binary(df)
-    print(df["time_for_free_cancellation"], df["weeks_to_checkin"])
     return df
+
 
 def classify_columns(df):
     return pd.get_dummies(df, columns=DUMMIES_COLUMNS, dtype=float)
 
-# def classifier_prediction(X, y):
-#     ensemble.RandomForestClassifier()
 
+def classifier_fit(X, y):
+    regr = ensemble.RandomForestRegressor(n_estimators=100, random_state=0)
+    regr.fit(X, y)
+    return regr
+
+def classifier_predict(X, regr):
+    return regr.predict(X)
+
+def classifier_loss():
+    pass
+
+def split_train_test(X: pd.DataFrame, y: pd.Series, train_proportion: float = 0.8):
+    train_X = X.sample(frac=train_proportion)
+    test_X = X.loc[X.index.difference(train_X.index)]
+    return train_X, y.loc[train_X.index], test_X, y.loc[test_X.index]
+
+
+def misclassification_error(y_true: np.ndarray, y_pred: np.ndarray, normalize: bool = True) -> float:
+    y_res = y_true - y_pred
+    false_response = np.count_nonzero(y_res)
+    if normalize:
+        return false_response / np.size(y_res)
+    return false_response
+max_depth = 30
 if __name__ == "__main__":
     np.random.seed(0)
     df = pd.read_csv("agoda_cancellation_train.csv")
     df = preprocess_data(df)
-    # y = create_cancellation_colunmn(df)
+    y = create_cancellation_colunmn(df)
+    X = df.drop(columns=["cancellation_datetime"])
+    train_X, train_y, train_cross_X, max_depth = max_depth, train_cross_y = split_train_test(X, y)
+    create_cancellation_colunmn(df)
+    regr = classifier_fit(train_X, train_y)
+    y_pred = classifier_predict(train_cross_X, regr)
+    y_pred = np.where(y_pred < 0.5, 0, 1)
+    print(misclassification_error(train_cross_y, y_pred))
 
-    # X = df.drop(columns=["cancellation_datetime"])
-    # a = 1
-    # create_cancellation_colunmn(df)
+
 
 
     # print(apply_booking_date(df))
